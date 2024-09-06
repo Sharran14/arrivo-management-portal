60241d4a3a5690d882c611042788a99db9cc8c5f
Sharran14 <sharran.35@gmail.com>
2024-09-06 19:01:26 +0800

    Final commit - Completed frontend assessment

--- lib/ui/post_list_screen.dart
+++ lib/ui/post_list_screen.dart
@@ -11,7 +11,7 @@ class PostListScreen extends StatefulWidget {
 class _PostListScreenState extends State<PostListScreen> {
   TextEditingController _searchController = TextEditingController();
   String _searchText = '';
-  int _currentPage = 0;
+  int _currentPage = 1;
   int _rowsPerPage = 10;
   List<int> _rowsPerPageOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
@@ -74,7 +74,7 @@ class _PostListScreenState extends State<PostListScreen> {
                           onChanged: (newValue) {
                             setState(() {
                               _rowsPerPage = newValue!;
-                              _currentPage = 0;
+                              _currentPage = 1; // Reset to the first page
                             });
                           },
                         ),
@@ -95,7 +95,7 @@ class _PostListScreenState extends State<PostListScreen> {
                         onChanged: (value) {
                           setState(() {
                             _searchText = value.toLowerCase();
-                            _currentPage = 0;
+                            _currentPage = 1; // Reset to the first page
                           });
                         },
                       ),
@@ -116,133 +116,198 @@ class _PostListScreenState extends State<PostListScreen> {
                                post.body.toLowerCase().contains(_searchText);
                       }).toList();
 
-                      // Pagination logic
-                      final totalPages = (filteredPosts.length / _rowsPerPage).ceil();
-                      final start = _currentPage * _rowsPerPage;
-                      final end = start + _rowsPerPage;
-                      final displayedPosts = filteredPosts.sublist(
-                          start, end > filteredPosts.length ? filteredPosts.length : end);
+                      // Pagination logic with clamp to prevent RangeError
+                      final start = ((_currentPage - 1) * _rowsPerPage).clamp(0, filteredPosts.length);
+                      final end = (start + _rowsPerPage).clamp(start, filteredPosts.length);
+                      final displayedPosts = filteredPosts.sublist(start, end);
 
                       return Column(
                         children: [
-                          SingleChildScrollView(
-                            scrollDirection: Axis.horizontal,
+                          // Make the table scrollable horizontally
+                          Expanded(
                             child: SingleChildScrollView(
-                              child: DataTable(
-                                columnSpacing: 20,
-                                columns: [
-                                  DataColumn(
-                                    label: Container(
-                                      width: 200,
-                                      child: Text('User ID'),
-                                    ),
-                                  ),
-                                  DataColumn(
-                                    label: Container(
-                                      width: 200,
-                                      child: Text('ID'),
-                                    ),
-                                  ),
-                                  DataColumn(
-                                    label: Container(
-                                      width: 500,
-                                      child: Text('Title'),
-                                    ),
-                                  ),
-                                  DataColumn(
-                                    label: Container(
-                                      width: 500,
-                                      child: Text('Body'),
-                                    ),
-                                  ),
-                                ],
-                                rows: displayedPosts.map((post) {
-                                  return DataRow(cells: [
-                                    DataCell(
-                                      Padding(
-                                        padding: EdgeInsets.symmetric(vertical: 12),
-                                        child: SizedBox(
-                                          width: 200,
-                                          child: Text(post.id.toString()),
+                              scrollDirection: Axis.horizontal, // Enable horizontal scrolling
+                              child: SingleChildScrollView(
+                                scrollDirection: Axis.vertical, // Enable vertical scrolling
+                                child: DataTable(
+                                  columnSpacing: 20,
+                                  columns: [
+                                    DataColumn(
+                                      label: Container(
+                                        alignment: Alignment.center, // Center-align header
+                                        padding: EdgeInsets.all(8.0), // Padding inside the header
+                                        child: Text(
+                                          'User ID',
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                          textAlign: TextAlign.center, // Center text inside the header
                                         ),
                                       ),
                                     ),
-                                    DataCell(
-                                      Padding(
-                                        padding: EdgeInsets.symmetric(vertical: 12),
-                                        child: SizedBox(
-                                          width: 200,
-                                          child: Text(post.id.toString()),
+                                    DataColumn(
+                                      label: Container(
+                                        alignment: Alignment.center,
+                                        padding: EdgeInsets.all(8.0),
+                                        child: Text(
+                                          'ID',
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                          textAlign: TextAlign.center,
                                         ),
                                       ),
                                     ),
-                                    DataCell(
-                                      Padding(
-                                        padding: EdgeInsets.symmetric(vertical: 12),
-                                        child: SizedBox(
-                                          width: 500,
-                                          child: Text(
-                                            post.title,
-                                            maxLines: 2,
-                                            overflow: TextOverflow.ellipsis,
+                                    DataColumn(
+                                      label: Container(
+                                        alignment: Alignment.center,
+                                        padding: EdgeInsets.all(8.0),
+                                        child: Text(
+                                          'Title',
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ),
+                                    ),
+                                    DataColumn(
+                                      label: Container(
+                                        alignment: Alignment.center,
+                                        padding: EdgeInsets.all(8.0),
+                                        child: Text(
+                                          'Body',
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                  rows: displayedPosts.map((post) {
+                                    return DataRow(
+                                      color: MaterialStateProperty.all<Color>(Colors.white), // White background for rows
+                                      cells: [
+                                        DataCell(
+                                          Padding(
+                                            padding: EdgeInsets.symmetric(vertical: 12),
+                                            child: SizedBox(
+                                              width: 200,
+                                              child: Text(post.id.toString()),
+                                            ),
                                           ),
                                         ),
-                                      ),
-                                    ),
-                                    DataCell(
-                                      Padding(
-                                        padding: EdgeInsets.symmetric(vertical: 12),
-                                        child: SizedBox(
-                                          width: 500,
-                                          child: Text(
-                                            post.body,
-                                            maxLines: 3,
-                                            overflow: TextOverflow.ellipsis,
+                                        DataCell(
+                                          Padding(
+                                            padding: EdgeInsets.symmetric(vertical: 12),
+                                            child: SizedBox(
+                                              width: 200,
+                                              child: Text(post.id.toString()),
+                                            ),
                                           ),
                                         ),
-                                      ),
-                                    ),
-                                  ]);
-                                }).toList(),
+                                        DataCell(
+                                          Padding(
+                                            padding: EdgeInsets.symmetric(vertical: 12),
+                                            child: SizedBox(
+                                              width: 500,
+                                              child: Text(
+                                                post.title,
+                                                maxLines: 2,
+                                                overflow: TextOverflow.ellipsis,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                        DataCell(
+                                          Padding(
+                                            padding: EdgeInsets.symmetric(vertical: 12),
+                                            child: SizedBox(
+                                              width: 500,
+                                              child: Text(
+                                                post.body,
+                                                maxLines: 3,
+                                                overflow: TextOverflow.ellipsis,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    );
+                                  }).toList(),
+                                ),
                               ),
                             ),
                           ),
-                          // Pagination controls
+                          // Pagination controls at the bottom right
                           Padding(
                             padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                IconButton(
-                                  icon: Icon(Icons.arrow_back),
-                                  onPressed: _currentPage > 0
-                                      ? () {
+                            child: Align(
+                              alignment: Alignment.bottomRight, // Aligns pagination to the bottom right
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end, // Aligns row contents to the end (right)
+                                children: [
+                                  IconButton(
+                                    icon: Icon(Icons.arrow_back_ios),
+                                    onPressed: _currentPage > 1
+                                        ? () {
+                                            setState(() {
+                                              _currentPage--;
+                                            });
+                                          }
+                                        : null,
+                                  ),
+                                  for (int i = 1; i <= (filteredPosts.length / _rowsPerPage).ceil(); i++) ...[
+                                    if (i == 1 || i == (filteredPosts.length / _rowsPerPage).ceil() || (i >= _currentPage - 1 && i <= _currentPage + 1))
+                                      GestureDetector(
+                                        onTap: () {
                                           setState(() {
-                                            _currentPage--;
+                                            _currentPage = i;
                                           });
-                                        }
-                                      : null,
-                                ),
-                                Text("Page ${_currentPage + 1} of $totalPages"),
-                                IconButton(
-                                  icon: Icon(Icons.arrow_forward),
-                                  onPressed: _currentPage < totalPages - 1
-                                      ? () {
-                                          setState(() {
-                                            _currentPage++;
-                                          });
-                                        }
-                                      : null,
-                                ),
-                              ],
+                                        },
+                                        child: Container(
+                                          padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                                          margin: EdgeInsets.symmetric(horizontal: 4),
+                                          decoration: BoxDecoration(
+                                            color: _currentPage == i ? Colors.purple : Colors.grey[300],
+                                            borderRadius: BorderRadius.circular(12),
+                                          ),
+                                          child: Text(
+                                            '$i',
+                                            style: TextStyle(
+                                              color: _currentPage == i ? Colors.white : Colors.black,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    if (i == _currentPage - 2 || i == _currentPage + 2)
+                                      Padding(
+                                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                                        child: Text("..."),
+                                      ),
+                                  ],
+                                  IconButton(
+                                    icon: Icon(Icons.arrow_forward_ios),
+                                    onPressed: _currentPage < (filteredPosts.length / _rowsPerPage).ceil()
+                                        ? () {
+                                            setState(() {
+                                              _currentPage++;
+                                            });
+                                          }
+                                        : null,
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         ],
                       );
                     } else if (state is PostError) {
                       return Center(child: Text('Failed to load posts'));
+                    } else {
+                      return Container(); // Empty container for other states
                     }
-                    return Container();
                   },
                 ),
               ),
